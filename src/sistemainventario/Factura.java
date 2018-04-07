9ddd02a758b14c9bc57bf3ce617f0b93c5be27d5
Jose Segura <00001617@uca.edu.sv>
2018-04-06 21:33:09 -0600

    Agregado la clase factura

---
+++ src/sistemainventario/Factura.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sistemainventario;
+
+/**
+ *
+ * @author Jose Segura
+ */
+public class Factura {
+    private String nombre;
+    private int cantidad;
+    private String fecha;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public String getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+    
+  
+}
